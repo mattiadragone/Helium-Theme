9c95f3b0c4088c62f1a0a5fd2c511ec0611302b8
Mattia Dragone <mattiadragone@outlook.it>
2022-11-02 15:49:48 +0100

    v 0.01 Header fix show on over

--- assets/details-disclosure.js
+++ assets/details-disclosure.js
@@ -47,4 +47,19 @@ class HeaderMenu extends DetailsDisclosure {
   }
 }
 
+let items = document.querySelector(".header__inline-menu").querySelectorAll("details");
+  console.log(items)
+  items.forEach(item => {
+    item.addEventListener("mouseover", () => {
+      item.setAttribute("open", true);
+      item.querySelector("ul").addEventListener("mouseleave", () => {
+        item.removeAttribute("open");
+      });
+    item.addEventListener("mouseleave", () => {
+      item.removeAttribute("open");
+    });
+  });
+  
+  });
+
 customElements.define('header-menu', HeaderMenu);
